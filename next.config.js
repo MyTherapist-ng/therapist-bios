ba33372b3824199ee81bae0b50866ec312e826a4
Ayoe <dannyhopkins20003@gmail.com>
2023-07-29 20:38:15 +0100

    refactor

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "https://mytherapist.ng/for-therapists",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://mytherapist.ng/for-therapists",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
