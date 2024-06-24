67243f69a9eb2389a2ba5b30502f084696d3c707
Oluwaseun Raphael Afolayan <theafolayan@gmail.com>
2024-06-24 18:08:25 +0100

    therapist pricing dynamic

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
           Ready to take action?
         </h1>
         <a className="font-semibold decoration-none text-base md:text-xl text-[#f5f6ff] mt-4 mb-8">
-          Book me for N5,000/hr
+          Book {user?.name} for as low as {user?.price?.display} / Session
         </a>
         <Button color="#1B1D3E" />
       </div>
