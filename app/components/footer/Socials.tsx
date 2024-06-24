e1ddac3a8170e59ca0b03f7fe92076d7b94febea
Oluwaseun Raphael Afolayan <theafolayan@gmail.com>
2024-06-24 20:30:21 +0100

    added proper social links

--- app/components/footer/Socials.tsx
+++ app/components/footer/Socials.tsx
@@ -9,7 +9,7 @@ const Socials = () => {
 
   return (
     <div className="flex space-x-6 mb-10 justify-center items-center">
-      <a href={user?.linked_url} target="_blank" className="hover:opacity-50 transition-all cursor-pointer">
+      <a href={`https://www.facebook.com/sharer/sharer.php?u=Checkout ${user?.name} on Mytherapistng https://${user?.username}.therapistfinder.xyz`} target="_blank" className="hover:opacity-50 transition-all cursor-pointer">
         <Image
           src={facebook}
           alt="facebook"
@@ -19,7 +19,18 @@ const Socials = () => {
         />
       </a>
 
-      <a href={user?.linked_url} target="_blank" className="hover:opacity-50 transition-all cursor-pointer">
+      <a href={`https://www.linkedin.com/feed/?shareActive=true&text=Checkout ${user?.name} on Mytherapist.ng https://${user?.name}.therapistfinder.xyz %23mytherapistng
+`} target="_blank" className="hover:opacity-50 transition-all cursor-pointer">
+        <Image
+          src={linkedIn}
+          alt="LinkedIn Share Icon"
+          width={36}
+          height={36}
+          className="md:w-12 md:h-12"
+        />
+      </a>
+
+      <a href={} target="_blank" className="hover:opacity-50 transition-all cursor-pointer">
         <Image
           src={linkedIn}
           alt="LinkedIn Share Icon"
