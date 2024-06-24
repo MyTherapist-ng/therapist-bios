1b3aea2aa418a53efb6e486773d0ae26db83aedd
Oluwaseun Raphael Afolayan <theafolayan@gmail.com>
2024-06-24 17:43:02 +0100

    added interview link

--- app/components/user/User.tsx
+++ app/components/user/User.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import Cta from "./Cta";
 import Userdetails from "./Userdetails";
 import UserProfile from "./UserProfile";
+import { useUser } from "@/app/context/userContext";
+
 
 const User = () => {
+  
+  const user = useUser();
   return (
     <div className="w-full flex flex-col justify-center items-center mt-[30px] md:mt-10 mb-[76px]">
       <UserProfile />
-      <p className="text-center underline font-medium text-sm md:text-base md:leading-8 text-grayscale cursor-pointer hover:opacity-60 transition-opacity">
+      <a href={user?.interview_url} className="text-center underline font-medium text-sm md:text-base md:leading-8 text-grayscale cursor-pointer hover:opacity-60 transition-opacity">
         Read interview
-      </p>
+      </a>
       <Userdetails />
       <Cta />
     </div>
