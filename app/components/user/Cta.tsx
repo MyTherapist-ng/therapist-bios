e88f8438b169bffb2a5bb86c927ba9cfcecbfd8b
Oluwaseun Raphael Afolayan <theafolayan@gmail.com>
2024-06-24 18:05:49 +0100

    fied interview url bug

--- app/components/user/Cta.tsx
+++ app/components/user/Cta.tsx
@@ -8,7 +8,7 @@ const Cta = () => {
   return (
     <div className="flex flex-col justify-center items-center md:flex-row space-y-2 md:space-y-0 md:space-x-3">
       <Button color="#1AB265" />
-      <a href={user.interview_url}>
+      <a href={user?.interview_url}>
       <button
         className="font-bold text-base text-grayscale leading-6 text-center bg-#FFFFFF border-grayscale
       border border-solid rounded-lg shadow-boxShadow w-[216px] h-[56px] hover:opacity-60 transition-all hover:scale-105 cursor-pointer">
