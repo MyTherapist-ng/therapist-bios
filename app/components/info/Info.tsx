100e79a6aae3be347ebd09b6583c83fef8454ad0
Oluwaseun Raphael Afolayan <theafolayan@gmail.com>
2024-07-22 17:12:43 +0100

    added video src

--- app/components/info/Info.tsx
+++ app/components/info/Info.tsx
@@ -7,7 +7,7 @@ import Reviews from "./Reviews";
 const Info = () => {
   return (
     <div className="w-full md:w-4/5 rounded-lg bg-[#f7f7f7] mb-7 px-4 md:px-[84px]">
-      {/* <Video /> */}
+      <Video />
       <Specialities />
       <Qualifications />
       <Reviews />
